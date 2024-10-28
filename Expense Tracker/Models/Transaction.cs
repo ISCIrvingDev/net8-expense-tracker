fe95acedb848bcd38516f046e69baf224eb44e77
Irving Salazar <ISCIrvingDev@gmail.com>
2024-10-28 13:23:13 -0600

    Category and Transaction models & ApplicationDbContext

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Note { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -1,3 +1,6 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Expense_Tracker
 {
     public class Program
@@ -8,6 +11,7 @@ namespace Expense_Tracker
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
             var app = builder.Build();
 
